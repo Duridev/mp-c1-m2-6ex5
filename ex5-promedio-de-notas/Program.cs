bb9e790e9158b23fdde1a6d84a5d89f87944bf5c
duridev <duridev@outlook.com>
2025-06-04 12:33:39 -0400

    desarrollo ejercicio 5 promedio de notas

--- ex5-promedio-de-notas/Program.cs
+++ ex5-promedio-de-notas/Program.cs
@@ -6,7 +6,21 @@ namespace ex5_promedio_de_notas
     {
         static void Main(string[] args)
         {
+            float nota1, nota2, nota3, promedio;
 
+            Console.WriteLine("Escribe tu primera nota:");
+            nota1 = float.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Escribe tu segunda nota:");
+            nota2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escribe tu tercera nota:");
+            nota3 = float.Parse(Console.ReadLine());
+
+            promedio = float.Parse((nota1 + nota2 + nota3) / 3, 1);
+            
+            Console.WriteLine($"El promedio final es: {promedio}")
         }
     }
 }
